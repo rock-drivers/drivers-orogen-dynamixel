1044449aacc5af87783da0fda20804890e931572
Janosch Machowinski <janosch.machowinski@dfki.de>
2011-03-24 18:33:53 +0100

    updated test scripts

--- scripts/test.rb
+++ scripts/test.rb
@@ -17,31 +17,51 @@ Orocos::Process.spawn 'test' do |p|
     Orocos.log_all_ports
 
     driver.device = ARGV[0]
-    driver.scanner_tilt_id = 1
-    driver.scanner_tilt_factor = 211.59 
-    driver.scanner_tilt_zero = 898.29 
+    driver.servo_id = 1
+    driver.zero_offset = 4.58660255577807
 
     driver.configure
     driver.start
 
     angle = 0.0
     #reader = driver.scanner_tilt_angle.reader(:type => :buffer, :size => 10)
-    reader = driver.scanner_tilt_angle.reader
+    reader = driver.angle.reader()
+  
+    sample = nil
+    while(!(sample = reader.read))
+      sleep 0.1
+    end
+
+    puts("#{sample/Math::PI*180.0} \r")
+  
+    read_angle = 0.0
 
     loop do
-	driver.set_scanner_tilt_angle( angle/180.0*Math::PI )
+	driver.setAngle( angle/180.0*Math::PI )
 
-	if sample = reader.read
-	    print("#{sample/Math::PI*180.0} \r")
-	end
+
+
+        while((read_angle/Math::PI * 180.0 - angle).abs > 2.0)
+          if sample = reader.read
+            read_angle = sample
+	    print("Cur Angle: #{sample/Math::PI*180.0} \r")
+          end
+          sleep 0.01
+        end
+
 	sleep 0.1
+        puts("")
 
+        puts('Enter next angle')
 	cmd = $stdin.readline.chomp
 	if cmd == "q"
 	    exit
 	else
 	    angle = cmd.to_i
+          puts("Setting angle to #{angle}")
 	end
+
+
     end
 end
 
